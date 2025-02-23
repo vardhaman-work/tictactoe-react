ae9a7ca35e9dee18c4c5e05b1ed50db5b252f0e4
vardhaman-work <vardhaman.work@gmail.com>
2025-02-23 21:41:59 +0530

    work on autoplay and other changes

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,7 +1,15 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useWindowSize } from 'react-use';
+import Confetti from 'react-confetti';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Modal from 'react-bootstrap/Modal';
+
 import "./TicTacToe.css";
 import circle_icon from "../Assets/circle.png";
 import cross_icon from "../Assets/cross.png";
+import Sound from "../Assets/sound.mp3";
 
 let data = ["","","","","","","","",""]
 
@@ -9,7 +17,21 @@ const TicTacToe = () => {
 
   let [count, setCount] = useState(0);
   let [lock, setLock] = useState(false);
+  let [matchWin, setMatchWin] = useState(false);
+  let [autoPlay, setAutoPlay] = useState(false);
   let titleRef = useRef(null);
+  const { width, height } = useWindowSize();
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    handleShow();
+  },[])
+
+  let audio = new Audio(Sound);
 
   let box1 = useRef(null);
   let box2 = useRef(null);
@@ -24,6 +46,7 @@ const TicTacToe = () => {
   let box_array = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
 
   const toggle = (e, num) => {
+
     if(lock){
       return 0
     }
@@ -39,10 +62,47 @@ const TicTacToe = () => {
     }
 
     e.target.classList.add('active');
-    //e.currentTarget.className += " active";
 
     checkWin();
+
+    if(autoPlay && e.isTrusted){
+      setTimeout(autoPlayHandler,1000)
+    }
   }
+
+  function NoneEmpty(arr) {
+    for(var i=0; i<arr.length; i++) {
+      if(arr[i] === "") return false;
+    }
+    return true;
+  }
+
+  function autoPlayHandler(){
+    if(!NoneEmpty(data)){
+
+      const emptyIndexes = data.reduce((acc, curr, index) => {
+        if (curr === '') {
+          acc.push(index);
+        }
+        return acc;
+      }
+      , [])
+      
+      const random = Math.floor(Math.random() * emptyIndexes.length);
+      const randomIndex = emptyIndexes[random];
+      
+      if(emptyIndexes?.length > 0 && !matchWin){
+        box_array.map((e,i) => {
+          if(i === randomIndex){
+            e.current.click();
+          }
+          return true;
+        })
+      }
+    }
+  }
+
+  
 
   const checkWin = () => {
     if(data[0]===data[1] && data[1]===data[2] && data[2]!==""){
@@ -61,17 +121,23 @@ const TicTacToe = () => {
       won(data[8], [0,4,8])
     } else if(data[2]===data[4] && data[4]===data[6] && data[6]!=="") {
       won(data[6], [2,4,6])
-    }
+    } else {
+      if(NoneEmpty(data)) {
+        titleRef.current.innerHTML = `Match is Draw`
+      }
+    }    
   }
 
   const won = (winner, arr) => {
     setLock(true);
+    setMatchWin(true);
+    audio.play();
 
     if(winner === "x") {
       titleRef.current.innerHTML = `Congratulations: <img src='${cross_icon}'/> wins`
     } else {
       titleRef.current.innerHTML = `Congratulations: <img src='${circle_icon}'/> wins`
-    }
+    }   
 
     box_array.map((e,i) => {
       if(arr.includes(i)){
@@ -83,6 +149,7 @@ const TicTacToe = () => {
 
   const reset = () => {
     setLock(false);
+    setMatchWin(false);
     data = ["","","","","","","","",""]
     titleRef.current.innerHTML = 'Tic Tac Toe Game in <span>React</span>';
 
@@ -93,11 +160,22 @@ const TicTacToe = () => {
     })
   }
 
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    
+    if(e.target.elements.autoplay.checked){
+      setAutoPlay(true);
+    }
+    
+    handleClose();
+  }
+
   return (
     <div className='container'>
       <h1 className="title" ref={titleRef}>
         Tic Tac Toe Game in <span>React</span>
       </h1>
+      
       <div className='board'>
         <div className="boxes" ref={box1} onClick={(e) => { toggle(e, 0) }}></div>
         <div className="boxes" ref={box2} onClick={(e) => { toggle(e, 1) }}></div>
@@ -110,6 +188,30 @@ const TicTacToe = () => {
         <div className="boxes" ref={box9} onClick={(e) => { toggle(e, 8) }}></div>        
       </div>
       <button className="reset" onClick={() => reset()}>Reset</button>
+      
+      <Modal show={show}>
+        <Modal.Header>
+          <Modal.Title>Setting</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={formSubmitHandler}>
+          <Row>
+            <Col>
+              <strong>AutoPlay</strong>
+            </Col>
+            <Col xs="auto">
+              <label className="switch">
+                <input name='autoplay' type="checkbox" />
+                <span className="slider round"></span>
+              </label>
+            </Col>
+            <Col xs={12} className='text-center border-top mt-3 pt-3'><Button type='submit' variant="primary">Save</Button></Col>
+          </Row>
+          </form>
+        </Modal.Body>
+      </Modal>
+
+      {matchWin && <Confetti width={width} height={height} numberOfPieces={500} />}
     </div>
   )
 }
